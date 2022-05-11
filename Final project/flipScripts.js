1754913fc5fcfecad7bee1be91f1d4a408a50403
sensen234 <101002380+sensen234@users.noreply.github.com>
2022-05-11 09:28:49 +0900

    FlipCard
    Demo

---
+++ Final project/flipScripts.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.cardElements');
+var card = cards.forEach
+
+function flipCard() {
+    console.log('clicked');
+};
+
+//cards.forEach(function(card){
+//    card.addEventlistener('click', flipCard
+// })
+//cards.forEach(card => card.addEventListener('click', flipCard));
